a221e3e8718f0ac56cc636c5d4d03e0b3dadc51b
Nikolas Papadopoulos <36321627+08Np08@users.noreply.github.com>
2018-03-25 18:19:14 -0400

    Update Get_batting_stats.py

--- Get_batting_stats.py
+++ Get_batting_stats.py
@@ -1,13 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Mar 19 00:49:15 2018
-@author: Nik
-"""
-
 import pandas as pd
 
-YEARS = 1
+YEARS = 3
 
 START_YEAR = 2017
 
@@ -16,12 +11,14 @@ COLUMNS = ["Rk","Gcar","Opp","AB","R","H","2B","3B",
            "OBP","SLG","OPS","BOP","aLI","WPA","RE24"]
 
 def main():
- 
+    names = []
     count = 0
+    
     batters_list = pd.DataFrame(columns = COLUMNS)
     batters = get_batters("batters.csv")
+
     
-    for i in range(len(batters):
+    for i in range(len(batters)):
         for j in range(YEARS):
             try:
                 count+=1
@@ -47,25 +44,31 @@ def main():
                                                                     -1,-2,-3]],
                                                                     axis=1)
             
+                for i in range(len(batter_list)):
+                    names.append(batter)
+                    
                 batters_list = batters_list.append(batter_list)
                 
-                print("percent finished",round(count/
+                batters_list.index = pd.RangeIndex(len(batters_list.index))
+    
+                drop_rows_by_equals(batters_list,"AB","R")
+    
+                drop_rows_by_equals(batters_list,"Gcar","Gtm")
+    
+                drop_rows_by_greater(batters_list,"AB",10)
+                
+                batters_list = batters_list.dropna(how="any", inplace = False)
+                
+                print("Percent Finished:",round(count/
                       (YEARS*len(batters))*100,2),"%")
-                print(batters_list.shape)
+
             except:
                 print("error")
                 continue
     
-    batters_list.index = pd.RangeIndex(len(batters_list.index))
+    names = pd.Series(names)
+    batters_list["Name"] = names
     
-    drop_rows_by_equals(batters_list,"AB","R")
-    
-    drop_rows_by_equals(batters_list,"Gcar","Gtm")
-    
-    drop_rows_by_greater(batters_list,"AB",10)
-    
-    batters_list = batters_list.dropna(how="any", inplace = False)
-    print(batters_list.shape)
     batters_list.to_pickle("batters_list")
     
 def get_batters(csv):
@@ -88,5 +91,5 @@ def drop_rows_by_greater(df,column,drop_criteria):
                 df.drop(df.index[i], inplace=True)     
         except:
             continue
-
+        
 main()
